8baa2cbbd95554c15d5a9d8c4350170254e6ad18
Marc Hermon <hermonm@urbandaleschools.com>
2018-11-18 20:57:13 -0600

    Create 5.1_Coin_Toss.py

---
+++ 5.1_Coin_Toss.py
@@ -0,0 +1,7 @@
+'''
+Coin Toss Program
+1.) Create a program that will print a random 0 or 1.
+2.) Instead of 0 or 1, print heads or tails. Do this using if statements. Don't select from a list.
+3.) Add a loop so that the program does this 50 times.
+4.) Create a running total for the number of heads and the number of tails and print the total at the end.
+'''
