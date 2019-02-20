d64bdcdc1a75141013c52ac53c2e201e01c63eab
Marc Hermon <hermonm@urbandaleschools.com>
2019-02-20 08:09:34 -0600

    Update 5.0_Take_Home_Test.py

--- 5.0_Take_Home_Test.py
+++ 5.0_Take_Home_Test.py
@@ -3,35 +3,8 @@ HONOR CODE: I solemnly promise that while taking this test I will only use PyCha
 but I will definitely not ask another person except the instructor. Signed: ______________________
 
 
-1. Write a Python program that will use a FOR loop to print your name
-     10 times, and then the word Done at the end.
- '''  
-
-
-'''
-  2. Write a Python program that will use a FOR loop to print Blue
-     and then White 20 times. (Blue White Blue White Blue White... all on separate lines.
-     Don't use \n.)
-'''
-
-
-'''
-  3. Write a Python program that will use a FOR loop to print the even
-     numbers from 2 to 100, inclusive.
-'''
-
-
-
-'''
-  4. Write a Python program that will use a WHILE loop to count from
-     10 down to, and including, 0. Then print the words Blast off! Remember, use
-     a WHILE loop, don't use a FOR loop.
-'''
-
-
-'''
-  5. There are three things wrong with this program. List each.
-     
+ 1. Make the following program work.
+   '''  
      print("This program takes three numbers and returns the sum.")
      total = 0
 
@@ -39,23 +12,40 @@ but I will definitely not ask another person except the instructor. Signed: ____
          x = input("Enter a number: ")
          total = total + i
      print("The total is:", x)
-'''
+  
 
 
 '''
-  6. Write a program that prints a random integer from 1 to 10 (inclusive).
+  2. Write a Python program that will use a FOR loop to print the even
+     numbers from 2 to 100, inclusive.
 '''
 
 
-'''
-  7. Write a program that prints a random floating point number somewhere between
-     1 and 10 (inclusive). Do not make the mistake of generating a random number from
-     0 to 10 instead of 1 to 10.
-'''
+
 
 
 '''
-  8. Write a Python program that will:
+  3. Write a program that will use a WHILE loop to count from
+     10 down to, and including, 0. Then print the words Blast off! Remember, use
+     a WHILE loop, don't use a FOR loop.
+'''
+
+
+
+
+
+
+'''
+  4. Write a program that prints a random integer from 1 to 10 (inclusive).
+'''
+
+
+
+
+
+
+'''
+  5. Write a Python program that will:
      
      * Ask the user for seven numbers
      * Print the total sum of the numbers
