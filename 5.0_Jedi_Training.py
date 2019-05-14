70848b32766222deffeabfbac6e5745978b47acf
Marc Hermon <hermonm@urbandaleschools.com>
2019-05-14 14:25:04 -0500

    Update and rename 5.0_Take_Home_Test.py to 5.0_Jedi_Training.py

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -1,8 +1,6 @@
+  #Sign your name:________________
+
 '''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
-
-
  1. Make the following program work.
    '''  
      print("This program takes three numbers and returns the sum.")
